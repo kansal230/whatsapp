45490515164ee320a8121dfbc0dd82c1873905ca
kansal230 <87700165+kansal230@users.noreply.github.com>
2026-08-11 17:13:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,12 @@ if uploaded_file is not None:
     user_list = df['user'].unique().tolist()
     user_list.sort()
     user_list.insert(0,'Overall')
-
     st.sidebar.selectbox('show analysis wrt',user_list)
+    selected_user = st.sidebar.selectbox('show analysis wrt',user_list)
+
+    if st.sidebar.button('show analysis'):
+      pass
+
     
 
 """To run a Streamlit application, you typically execute a shell command. Based on the output from your previous cell, you can run your Streamlit app using the following command in a new code cell:"""
