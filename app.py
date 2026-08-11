d89ce6bba4b044b4d2c446e6893b4b14cf8a7843
kansal230 <87700165+kansal230@users.noreply.github.com>
2026-08-11 17:09:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ if uploaded_file is not None:
     user_list = df['user'].unique().tolist()
 
     st.sidebar.selectbox('show analysis wrt',user_list)
-    user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0,'Overall')
 
